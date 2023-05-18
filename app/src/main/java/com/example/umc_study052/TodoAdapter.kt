33bf2129e93660696c9b3d1c31a0d634a17eaced
kangyuri1114 <yuri3334@gmail.com>
2023-05-19 02:42:10 +0900

    RecyclerView 스위치 재사용 오류 해결

--- app/src/main/java/com/example/umc_study052/TodoAdapter.kt
+++ app/src/main/java/com/example/umc_study052/TodoAdapter.kt
@@ -30,6 +30,11 @@ class TodoAdapter(private val itemList: List<Todo>) :
 
         fun bind(item: Todo) {
             binding.todoText.text = item.text
+            binding.switch1.setOnCheckedChangeListener(null) // 기존 리스너 제거
+
+            // 아이템 데이터의 isChecked 값에 따라 스위치 상태 설정
+            binding.switch1.isChecked = item.isChecked
+
             binding.switch1.setOnCheckedChangeListener { _, isChecked ->
                 item.isChecked = isChecked //값 변경 시 마다 isChecked 업데이트
             }
